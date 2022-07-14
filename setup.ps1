a0c9d89a2446af36bb1e7920b3edf1f67320c819
bus1hero <petru.cervac@gmail.com>
2022-07-14 20:20:06 +0300

    Add setup.ps1
    Add a script to setup generate a module manifest file

--- setup.ps1
+++ setup.ps1
@@ -1,4 +1,8 @@
 New-ModuleManifest `
 	-Path "${PSScriptRoot}\src\Pester.Extenssions.psd1" `
-	-Author "Petru Cervac" `
-	-ProjectUri 'https://github.com/BusHero/pester.extenssions'
+	-Author 'Petru Cervac' `
+	-ProjectUri 'https://github.com/BusHero/pester.extenssions' `
+	-LicenseUri 'https://github.com/BusHero/pester.extenssions/blob/main/license' `
+	-CmdletsToExport .\src\Get-ScriptPath.ps1 `
+	-Tags Pester, Tests `
+	-ModuleVersion '0.1.0'
