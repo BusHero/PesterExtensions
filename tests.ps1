5e95e6b49a6da7714845f22bcd5eb27564520071
bus1hero <petru.cervac@gmail.com>
2022-07-19 03:59:18 +0300

    Make tests.ps1 more sophisticated
    Add 2 containers in the tests.ps1 file

--- tests.ps1
+++ tests.ps1
@@ -2,8 +2,13 @@ param(
 	[int]$RunNumber
 )
 
-$container = New-PesterContainer `
-	-Path . `
+$container0 = New-PesterContainer `
+	-Path '.\tests\PesterExtensions\Public'
+
+$container1 = New-PesterContainer `
+	-Path '.\tests\PesterExtensions\PesterExtensions.Tests.ps1' `
 	-Data @{ Revision = $RunNumber }
 
-Invoke-Pester -Container $container
+
+Invoke-Pester -Container $container0 
+Invoke-Pester -Container $container1 
