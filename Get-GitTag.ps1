f748db138ac37317064609d5a2ba785450e86579
petru <petru.cervac@amdaris.com>
2022-07-21 08:13:01 +0300

    Update Get-GitTag.ps1

--- Get-GitTag.ps1
+++ Get-GitTag.ps1
@@ -4,6 +4,6 @@ param (
 )
 
 git fetch --prune
-$foo = git describe --tags --abbrev=0 --match 'v*' --exclude '*-rc*' 'HEAD~'
+$foo = git describe --tags --abbrev=0 --match 'v*' --exclude '*-rc*'
 $foo = $foo.substring(1)
 return "::set-output name=$Tag::$foo"
