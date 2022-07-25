860b34908e5617cc95c7b593b73da71bb71906fc
petru <petru.cervac@amdaris.com>
2022-07-25 17:16:00 +0300

    feat: Update Mock-EnvironmentVariable
    Implement the basics for the Mock-EnvironmentVariable

--- src/PesterExtensions/Public/Mock-EnvironmentVariable.ps1
+++ src/PesterExtensions/Public/Mock-EnvironmentVariable.ps1
@@ -3,17 +3,30 @@ function Mock-EnvironmentVariable {
 		[parameter(Mandatory = $true, Position = 0)]
 		[string]
 		$Variable,
-
 		
 		[Parameter(Mandatory = $false)]
 		[string]
 		$Value,
 
-
 		[Parameter(Mandatory = $true, Position = 1)]
 		[ScriptBlock]
 		$Fixture
 	)
+	if (Test-Path -Path "env:${Variable}") {
+		$OriginalValue = (Get-ChildItem -Path "env:${Variable}").Value
+		Set-Item -Path "env:${Variable}" -Value $Value
+	}
+	else {
+		New-Item -Path "env:${Variable}" -Value $Value
+	}
+	Invoke-Command -ScriptBlock $Fixture
+
+	if ($OriginalValue) {
+		Set-Item -Path "env:${Variable}" -Value $OriginalValue
+	}
+	else {
+		Remove-Item -Path "env:${Variable}"
+	}
 
 	<#
 	.PARAMETER Variable
--- tests/PesterExtensions/Public/Mock-EnvironmentVariable.Tests.ps1
+++ tests/PesterExtensions/Public/Mock-EnvironmentVariable.Tests.ps1
@@ -1,5 +1,53 @@
 BeforeAll {
 	Import-Module -Name PesterExtensions
-	. Get-ScriptPath -Path $PSCommandPath
+	. "$(Get-ScriptPath -Path $PSCommandPath)"
 	Remove-Module -Name PesterExtensions
+}
+
+Describe 'Mock an environment variable' {
+	BeforeAll {
+		$script:environmentVariable = "test$(New-Guid)"
+		$script:InitialValue = 'Some value here and there'
+
+	}
+	It 'Code is called' {
+		$script:environmentVariable = "test$(New-Guid)"
+		$script:InitialValue = 'Some value here and there'
+		$script:called = $false
+		Mock-EnvironmentVariable -Variable $environmentVariable -Value $InitialValue {
+			$script:called = $true
+		}
+		$script:called | Should -BeTrue
+	}
+
+	It 'Environment variable is set up' {
+		$environmentVariable = "test$(New-Guid)"
+		$InitialValue = 'Some value here and there'
+		Test-Path -Path $environmentVariable | Should -BeFalse 
+		Mock-EnvironmentVariable -Variable $environmentVariable -Value $InitialValue {
+			$foo = Get-ChildItem -Path "env:${environmentVariable}"
+			$foo.Value | Should -Be $InitialValue
+		}
+		Test-Path -Path $environmentVariable | Should -BeFalse 
+	}
+
+	Describe 'asd' {
+		BeforeAll {
+			$environmentVariable = "test$(New-Guid)"
+			$InitialValue = 'Some value here and there'
+			$UpdatedValue = 'Some updated value'
+			New-Item -Path "env:${environmentVariable}" -Value $InitialValue
+		}
+		It 'Environment variable is set up' {
+			Mock-EnvironmentVariable -Variable $environmentVariable -Value $UpdatedValue {
+				$foo = Get-ChildItem -Path "env:${environmentVariable}"
+				$foo.Value | Should -Be $UpdatedValue
+			}
+			$bar = Get-ChildItem -Path "env:${environmentVariable}"
+			$bar.Value | Should -Be $InitialValue
+		}
+		AfterAll {
+			Remove-Item -Path "env:${environmentVariable}" -Force -Recurse -ErrorAction Ignore
+		}
+	}
 }
